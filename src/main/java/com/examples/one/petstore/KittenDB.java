89ef9b3c397388d310758b7683c81e3e3ecd14ca
Daniel Catania <dcatania318@gmail.com>
2016-03-14 09:36:32 +0100

    added KittenDB.java

--- src/main/java/com/examples/one/petstore/KittenDB.java
+++ src/main/java/com/examples/one/petstore/KittenDB.java
@@ -1,5 +1,36 @@
 package com.examples.one.petstore;
+import java.util.ArrayList;
 
-public class KittenDB {
-
+public class KittenDB
+{
+	public ArrayList<Kitten> kittenList = new ArrayList<Kitten>(5);
+	
+	public boolean addKitten(Kitten k)
+	{
+		return kittenList.add(k);
+	}
+	public boolean delKitten(int id_in)
+	{
+		int index = -1;
+		for (int i=0; i<kittenList.size(); i++)
+		{
+			if (kittenList.get(i).id == id_in)
+				index = i;
+		}
+		return kittenList.remove(kittenList.get(index));
+	}
+	public Kitten getKitten(int id_in)
+	{
+		int index = -1;
+		for (int i=0; i<kittenList.size(); i++)
+		{
+			if (kittenList.get(i).id == id_in)
+				index = i;
+		}
+		return kittenList.get(index);
+	}
+	public int countKittens()
+	{
+		return kittenList.size();
+	}
 }
