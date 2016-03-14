a29091a0cf8da2197384d648db0815c337dc3acb
Michael Calleja <michaelcalleja95@gmail.com>
2016-03-14 09:14:02 +0100

    Added properties and methods for Puppy

--- src/main/java/com/examples/one/petstore/Puppy.java
+++ src/main/java/com/examples/one/petstore/Puppy.java
@@ -1,5 +1,43 @@
 package com.examples.one.petstore;
 
-public class Puppy {
-
+public class Puppy 
+{
+	public int id;
+	public String name;
+	public String breed;
+	public boolean loudBark;
+	public int exerciseRequiredPerDay;
+	public boolean friendlyWithChildren;
+	
+	public Puppy(int c_id, String c_name, String c_breed)
+	{
+		id = c_id;
+		name = c_name;
+		if(("Labrador".equals(c_breed)) ||
+				("Pitbull".equals(c_breed)) || 
+				("Doberman".equals(c_breed)) )
+		{
+			breed = c_breed;
+		}
+		else
+		{
+			breed = "Labrador";
+		}
+	}
+	
+	public int getID()
+	{
+		return id;
+	}
+	
+	public String getBreed()
+	{
+		return breed;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
 }
